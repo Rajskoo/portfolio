0b4e8c02171ed376df6d21d5c753cadf48504375
Rajskoo <62794779+Rajskoo@users.noreply.github.com>
2023-01-10 18:33:52 +0100

    Updated dark mode + bugfixes

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+const getPreferredColorScheme = () => {
+    const darkQuery = "(prefers-color-scheme: dark)";
+    const darkMQL = window.matchMedia ? window.matchMedia(darkQuery) : {};
+    if (darkMQL.media === darkQuery && darkMQL.matches) {
+      return "dark";
+    }
+  
+    return "default";
+  };
+  
+  const colorScheme =
+    localStorage.getItem("color-scheme") || getPreferredColorScheme();
+  document.documentElement.setAttribute("data-color-scheme", colorScheme);
+
+  document.getElementById("button").onclick = () => {
+    const colorScheme = document.documentElement.getAttribute(
+      "data-color-scheme"
+    );
+    const newColorScheme = colorScheme === "default" ? "dark" : "default";
+    document.documentElement.setAttribute("data-color-scheme", newColorScheme);
+  
+    localStorage.setItem("color-scheme", newColorScheme);
+  };
